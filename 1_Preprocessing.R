03e76640d8fb6ab74975f719816a6b0e1a908c40
ernestotruqui <etruqui@uchicago.edu>
2022-01-31 11:58:44 -0600

    Update 1_Preprocessing.R

--- 1_Preprocessing.R
+++ 1_Preprocessing.R
@@ -32,22 +32,22 @@ library(expss)
 # Read Data ####
 
 ## crime data ####
-df_crime <- read_xlsx(file.path(path, "Data_Manzana_MDE.xlsx"))[, c(4, 37:68)]
+df_crime <- read_xlsx(file.path(PATH, "Data_Manzana_MDE.xlsx"))[, c(4, 37:68)]
 
 ## shape file ####
 #df_shp <- st_read(file.path(PATH, "MGN_ANM_MANZANA.shp"))
-df_shp <- st_read(file.path(path, "manzanas_MEVAL.shp"))[, 1:106]
+df_shp <- st_read(file.path(PATH, "manzanas_MEVAL.shp"))[, 1:106]
 
 # Clean Data ####
 ## add labels ####
 # create function
-add_labels <- function(df, dict_filename, path=PATH){
+add_labels <- function(df, dict_filename, path = PATH){
   
   # load library
   library(labelled)
   
   # read dictionary
-  dict <- read.csv(file.path(path, dict_filename))
+  dict <- read.csv(file.path(PATH, dict_filename))
   colnames(dict) <- c('colname','label')
   # create named list
   nlist <- dict$label
@@ -64,15 +64,15 @@ add_labels <- function(df, dict_filename, path=PATH){
 }
 
 # run function
-df_shp <- add_labels(df_shp,'shapefile_labels.csv')
+df_shp <- add_labels(df_shp, "shapefile_labels.csv")
 df_crime <- add_labels(df_crime,'crime_labels.csv')
 
 ## merge data####
 
 # drop useless cols
-select_cols <- function(df,dict_filename,path=PATH){
-  dict_colname <- read_excel(file.path(path,dict_filename))
-  df <- df[,dict_colname$Variable]
+select_cols <- function(df, dict_filename, path=PATH){
+  dict_colname <- read_excel(file.path(path, dict_filename))
+  df <- df[, dict_colname$Variable]
 }
 df_shp <- select_cols(df_shp,'Selected data dictionary.xlsx')
 
@@ -81,12 +81,4 @@ colnames(df_crime)[1] <- colnames(df_shp)[1]
 
 # merge
 df_main <- merge(df_shp, df_crime, by = colnames(df_shp)[1])
-df_main_all <- merge(df_shp, df_crime, by = colnames(df_shp)[1],all=T)
 
-## check for NAs####
-sum(is.na(df_main_all$hom2012)) - sum(is.na(df_main$hom2012))
-# missing crime data for 4128 blocks!
-# distribution of blocks without crime data
-ggplot(df_main_all)+
-  geom_sf(aes(fill = is.na(hom2012))) +
-  ggtitle('Blocks Without Crime Data')
