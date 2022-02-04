cb59121877f8fc2de5ab71ca054703788ec0914e
ernestotruqui <etruqui@uchicago.edu>
2022-02-03 18:10:17 -0600

    Update crime_time.R

--- crime_time.R
+++ crime_time.R
@@ -4,19 +4,33 @@ library(readxl)
 
 path <- "C:/Users/52322/OneDrive - The University of Chicago/Documents/Harris/2022 Winter/Policy Lab/Data/Data"
 
-df_homicides19 <- read_xlsx(file.path(path, "crimes_times.xlsx"), sheet = 4)
+quadrants <- st_read(file.path(path, "lines.shp"))
+
+df_homicides19 <- read_csv(file.path(path, "homicides_2019.csv"))
 df_homicides19 <- df_homicides19 %>%
   mutate(lat = ifelse(str_detect(lat, ","), str_replace(lat, pattern = ",", replacement = "."), lat),
          lat = as.numeric(lat),
-         lon = ifelse(str_detect(lon, ","), str_replace(lon, pattern = ",", replacement = "."), lon),
+         lon = gsub(pattern = "^(.{3})(.*)$", replacement = "\\1.\\2", x = lon),
          lon = as.numeric(lon)) %>%
   st_as_sf(coords = c("lon", "lat"), crs = 4326)
 
-quadrants <- st_read(file.path(path, "lines.shp"))
+df_theft19 <- read_csv(file.path(path, "theft_2019.csv"))
+df_theft19 <- df_theft19 %>%
+  st_as_sf(coords = c("lon", "lat"), crs = 4326)
+df_burglary19 <- read_csv(file.path(path, "burglary_2019.csv"))
+df_burglary19 <- df_burglary19 %>%
+  st_as_sf(coords = c("lon", "lat"), crs = 4326)
+df_vtheft19 <- read_csv(file.path(path, "vehicle_theft_2019.csv"))
+df_vtheft19 <- df_vtheft19 %>%
+  mutate(lat = ifelse(str_detect(lat, ","), str_replace(lat, pattern = ",", replacement = "."), lat),
+         lat = as.numeric(lat),
+         lon = gsub(pattern = "^(.{3})(.*)$", replacement = "\\1.\\2", x = lon),
+         lon = as.numeric(lon)) %>%
+  st_as_sf(coords = c("lon", "lat"), crs = 4326)
 
 
 
 
 ggplot() +
-  #geom_sf(data = quadrants) +
+  #geom_sf(data = quadrants) #+
   geom_sf(data = df_homicides19)
