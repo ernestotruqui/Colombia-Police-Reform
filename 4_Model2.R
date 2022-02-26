a6eb8288b2c41e3938b78c8c3446218375a793c8
ernestotruqui <etruqui@uchicago.edu>
2022-02-26 16:55:13 -0600

    Update 4_Model2.R

--- 4_Model2.R
+++ 4_Model2.R
@@ -138,79 +138,13 @@ into_final_df <- function(crit_value){
 
 df_all <- into_final_df(crit_value = 10)
 
-
-
-
-manrique <- df_shifts_avg %>%
-  filter(station == "MANRIQUE")
-
-manrique_morn <- manrique %>%
-  filter(shift == "5-13")
-manrique_aftn <- manrique %>%
-  filter(shift == "13-21")
-manrique_nght <- manrique %>%
-  filter(shift == "21-5")
-
-manrique_morn %>% 
-  st_drop_geometry() %>%
-  group_by(region) %>%
-  dplyr::summarise(crime_sum = sum(sum))
-
-# source: https://stackoverflow.com/questions/45497742/generate-all-possible-combinations-of-rows-in-r
-matrix <- expand.grid(quad_1 = manrique_morn$region, quad_2 = manrique_morn$region)
-
-matrix[, paste("teacher", c("height", "smart"), sep="_")] <- 
-  teachers[match(res$teacher_name, teachers$name), c("height","smart")]
-
-
-
-
-
-
-
-
-
-
-
-
-
-filter_pairs <- function(df_pairs, scode){
-  df_temp <- df_pairs %>% filter(grepl(scode, from))
-  df_temp <- df_temp %>% filter(grepl(scode, to))
-  return(df_temp)
+# function to indicate which quads to merge together
+which_to_merge <- function(){
+  to_merge <- df_all %>%
+    rename(merge_with = to) %>%
+    select(from, shift, merge_with, geometry_to)
+  df_final <- left_join(df_shifts_avg, to_merge, by = c("region" = "from", "shift"))
 }
 
-df_d1e1 <- filter_pairs(df_pairs,'D01E01')
-
-sum_pairs <- function(df_pairs,df_shift){
-  df_temp <- df_pairs %>%
-    left_join(st_drop_geometry(df_shift[,c('region','sum')]),by = c("from" = "region"))
-  colnames(df_temp)[3] = 'sum0'
-  df_temp <- df_temp %>%
-    left_join(st_drop_geometry(df_shift[,c('region','sum')]),by = c("to" = "region"))
-  colnames(df_temp)[4] = 'sum1'
-  df_temp$sum <- df_temp$sum0 + df_temp$sum1
-  return(df_temp)
-}
-
-manrique_morn_pairs <- sum_pairs(df_d1e1, manrique_morn) %>%
-  select("from", "to", "sum") %>%
-  arrange(sum)
-manrique_aftn_pairs <- sum_pairs(df_d1e1, manrique_aftn) %>%
-  select("from", "to", "sum") %>%
-  arrange(sum)
-manrique_nght_pairs <- sum_pairs(df_d1e1, manrique_nght) %>%
-  select("from", "to", "sum") %>%
-  arrange(sum)
-
-ggplot() +
-  geom_sf(data = manrique_morn,
-          aes(fill = sum))
-ggplot() +
-  geom_sf(data = manrique_aftn,
-          aes(fill = sum))
-ggplot() +
-  geom_sf(data = manrique_nght,
-          aes(fill = sum))
-
-
+df_final <- which_to_merge()
+  
